92ac9415433aba086fb6303c7f952e38dfb1ed45
reggaeguitar <reggaeguitar@users.noreply.github.com>
2015-06-03 14:09:40 -0500

    Declared i in each for loop, moved incrementing of flowers bought to it's own line

--- hrFlowersC/main.c
+++ hrFlowersC/main.c
@@ -11,12 +11,12 @@ int comp_desc(const void * a, const void * b)
 
 int main()
 {
-	int flowersNeeded, numFriends, i;
+	int flowersNeeded, numFriends;
 	scanf("%d %d", &flowersNeeded, &numFriends);
 	getchar();
 	int flowerCosts[100];
 	memset(flowerCosts, 0, sizeof(flowerCosts));
-	for (i = 0; i < flowersNeeded; ++i)
+	for (int i = 0; i < flowersNeeded; ++i)
 	{
 		scanf("%d", &flowerCosts[i]);
 	}
@@ -24,10 +24,11 @@ int main()
 	int flowersBought = 0;
 	int moneySpent = 0;
 	int multiplier = 1;
-	for (i = 0; i < flowersNeeded; ++i)
+	for (int i = 0; i < flowersNeeded; ++i)
 	{
 		moneySpent += flowerCosts[i] * multiplier;
-		multiplier = (++flowersBought / numFriends) + 1;
+		++flowersBought;
+		multiplier = (flowersBought / numFriends) + 1;
 	}
 	printf("%d\n", moneySpent);
 	return 0;
